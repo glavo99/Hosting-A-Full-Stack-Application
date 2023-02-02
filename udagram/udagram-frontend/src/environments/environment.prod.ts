a3b767a97f9975911d65fd9b07e6fd9bcbb9a7c4
Yousef Elgendy <113210670+glavo99@users.noreply.github.com>
2023-02-02 19:28:57 +0200

    Update environment.prod.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-env.eba-darcpr2f.us-east-1.elasticbeanstalk.com',
+  apiHost: 'http://udagram-api-env.eba-darcpr2f.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
